048b579a9f6b8bf56196f58f40393a65a2a5f1dd
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-11 14:57:35 +0530

    DOM Manipulation in JavaScript
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,45 +1,10 @@
-/* Objects in JS */
+/* DOM manipulations & getElementById method */
 
-var Car = {
-    car_brand : "Tesla",
-    car_model : "Model 3",
-    price : "$35000",
-    autoPilot : function() {
-        return "This car has auto pilot";
-    }
+// function btnClick() {
+//     alert("Button Clicked");
+// }
+
+function btnClick() {
+    document.getElementById("heading2")
+    .innerHTML="Satish Kumar Gupta";
 }
-
-document.write("<h2>"+Car.car_brand +"</h2>");
-document.write("<h2>"+Car.autoPilot()+"</h2>");
-
-document.write("___________________________________________________");
-
-function Cars(car_brand, car_model, price) {
-    this.car_brand = car_brand;
-    this.car_model = car_model;
-    this.price = price;
-    this.autoPilot = function() {
-        document.write("<h2>This car has auto pilot</h2>");
-    }
-}
-
-var car1 = new Cars("Volvo", "Model 2", "$32000");
-document.write("<h2>"+car1.car_brand+"</h2>");
-document.write("<h2>"+car1.price+"</h2>");
-car1.autoPilot();
-
-document.write("___________________________________________________");
-
-var str = new String();
-document.write("<h2>"+typeof(str)+"</h2>");
-
-var str1 = "String";
-document.write("<h2>"+typeof(str1)+"</h2>");
-
-document.write("___________________________________________________");
-
-Car.fuelType = "Electric";
-document.write("<h2>"+Car.fuelType+"</h2>");
-
-delete Car.price;
-document.write("<h2>"+Car.price+"</h2>");
