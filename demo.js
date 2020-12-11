a5a012258da2802b596911e49aa9294a10a2c1e3
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-11 14:30:30 +0530

    Introduction to Objects
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,23 +1,31 @@
-var cars = ["BMW","Volvo","Audi"];
+/* Objects in JS */
 
-for (let index = 0; index < cars.length; index++) {
-    
-    document.write("<h4>"+cars[index]+"</h4>");
-    
+var Car = {
+    car_brand : "Tesla",
+    car_model : "Model 3",
+    price : "$35000",
+    autoPilot : function() {
+        return "This car has auto pilot";
+    }
 }
 
-document.write("________________________________________________");
+document.write("<h2>"+Car.car_brand +"</h2>");
+document.write("<h2>"+Car.autoPilot()+"</h2>");
 
-var arr;
-arr = new Array("Mango", "Apple", "Banana");
+document.write("___________________________________________________");
 
-arr.push("Grapes");
-arr.push(90);
-
-for (let index = 0; index < arr.length; index++) {
-    
-    document.write("<h4>"+arr[index]+"</h4>");
-    
+function Cars(car_brand, car_model, price) {
+    this.car_brand = car_brand;
+    this.car_model = car_model;
+    this.price = price;
+    this.autoPilot = function() {
+        document.write("<h2>This car has auto pilot</h2>");
+    }
 }
+
+var car1 = new Cars("Volvo", "Model 2", "$32000");
+document.write("<h2>"+car1.car_brand+"</h2>");
+document.write("<h2>"+car1.price+"</h2>");
+car1.autoPilot();
 
 
