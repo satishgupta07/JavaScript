07ba4b95c0d6a769bbdfdbe830e36da2540b7a20
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-11 11:25:01 +0530

    Variables and Data Types
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,1 +1,16 @@
 document.write("<h1> External JS File </h1>");
+
+
+// JS is LOOSELY TYPED
+// JS is DYNAMICALLY TYPED
+
+var num = 5;    //Number
+var name = "Satish Kumar Gupta";    //String
+var flag = true;      //Boolean
+num = "Rahul Gupta";
+
+var x; //declaration 
+x = 16; //initialization
+
+document.write(num);
+alert(name);
