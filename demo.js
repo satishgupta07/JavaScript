4490b37f98936bb538ff461d0a21e066b7757839
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-11 12:45:43 +0530

    While Loop
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,14 +1,10 @@
 /*
-  Q1.) Print table of 5 using for loop
-  Q2.) Print first 5 even numbers // 2 4 6 8 10
+  Q1.) Print first 5 odd numbers using while loop
 */
 
 // entry controlled loop
-// for(var x=1; x<=10 ; x++) {
-//     document.write("<h2>"+5*x+"</h2>");
-// }
-
-var x=2 
-for(;x<=10;x+=2) {
-    document.write("<h2>"+(x)+"</h2>");
+var x = 1;
+while(x<10) {
+    document.write("<h3>"+x+"</h3>");
+    x=x+2;
 }
