a37e0c130acb83d24ba174356ff83a4fd41b092b
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-11 14:43:12 +0530

    Objects
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -28,4 +28,18 @@ document.write("<h2>"+car1.car_brand+"</h2>");
 document.write("<h2>"+car1.price+"</h2>");
 car1.autoPilot();
 
+document.write("___________________________________________________");
 
+var str = new String();
+document.write("<h2>"+typeof(str)+"</h2>");
+
+var str1 = "String";
+document.write("<h2>"+typeof(str1)+"</h2>");
+
+document.write("___________________________________________________");
+
+Car.fuelType = "Electric";
+document.write("<h2>"+Car.fuelType+"</h2>");
+
+delete Car.price;
+document.write("<h2>"+Car.price+"</h2>");
