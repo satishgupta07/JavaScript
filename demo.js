17969a43aae8b3fcc0764665c6dec29e92f4a8af
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-11 12:10:29 +0530

    If-Else Control Statements
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,11 +1,33 @@
-// Associativity
-var a = 3; // right-to-left
-var b = 5;
-var c = 7;
-var result = a+c+"Satish"+a+b;  // left to right
+/*
+   Q1.) Find whether a number is even or odd 
+   Q2.) Find if a number is positive, negative or 0
+   Q3.) Find if a number is positive and even
+*/
 
-// Operator Precedence
-var result2 = c+b*a;
+document.write("<h1>If-ELse Control Statements </h1>");
 
-document.write("<h1>Result is : "+result+"</h1>");
-document.write("<h1>Result is : "+result2+"</h1>");
+var x = 10;
+
+// if(x%2==0) {
+//     document.write("<h3>EVEN NUMBER </h3>");
+// }
+// else {
+//     document.write("<h3>ODD NUMBER </h3>");
+// }
+
+// if(x>0) {
+//     document.write("<h3>POSITIVE NUMBER </h3>");
+// }
+// else if(x<0) {
+//     document.write("<h3>NEGATIVE NUMBER </h3>");
+// }
+// else {
+//     document.write("<h3>ZERO </h3>");
+// }
+
+    if(x>0 && x%2==0) {
+        document.write("<h3>POSITIVE AND EVEN NUMBER </h3>");
+    }
+    else if(x<0) {
+        document.write("<h3>NEGATIVE NUMBER </h3>");
+    }
