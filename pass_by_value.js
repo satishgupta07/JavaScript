8a561266fa067d7302573d952bfb436f5472f3c0
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-16 14:59:41 +0530

    Pass by Value and Pass by Reference
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ pass_by_value.js
@@ -0,0 +1,31 @@
+var quidditchWinner = "Harry Potter";
+
+function changeWinner(winner) {
+    console.log("Original Winner = "+winner);
+    winner = "Draco Malfoy"
+    console.log("Changed Winner = "+winner);
+}
+
+console.log("The winner was "+quidditchWinner);
+
+changeWinner(quidditchWinner);
+
+console.log("Now the winner is "+quidditchWinner);
+
+console.log("------------------------- Pass By Ref ----------------------------");
+
+var quidditchWinners = [
+    "Harry", "Ron", "Hermoine"
+];
+
+function changeWinner(winners) {
+    winners[0] = "Draco";
+    winners[1] = "Crabbe";
+    winners[2] = "Pansy";
+}
+
+console.log("The winners were "+quidditchWinners);
+
+changeWinner(quidditchWinners);
+
+console.log("Now the winner is "+quidditchWinners);
