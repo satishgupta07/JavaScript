91fc71d7be343766e41e49aed9c41daaeb296606
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-16 16:31:07 +0530

    CallStack in JavaScript
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

---
+++ callstack.js
@@ -0,0 +1,26 @@
+function ceo() {
+    console.log("let's make a dent in the universe")
+    cto()
+}
+
+function cto() {
+    console.log("let's make a gamechanging product")
+    vpengg()
+}
+
+function vpengg() {
+    console.log("let's make a new JavaScript Framework")
+    techlead()
+}
+
+function techlead() {
+    console.log("let's convert Angular to Typescript")
+    developer()
+}
+
+function developer() {
+    console.log("let's copy some code from StackOverFlow")
+    // throw new Error("The code didn't work")
+}
+
+ceo();
