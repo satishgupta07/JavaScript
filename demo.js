b8d03b87ce883cbe88b663c86dbe728c44bdd665
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-16 13:05:26 +0530

    Form Validation in JavaScrip Part-2
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -4,12 +4,22 @@ function validate() {
     var username = document.getElementById("uname");
     var password = document.getElementById("pass");
 
-    if(username.value.trim() == "" || password.value.trim() == "") {
-        alert("No blank values is allowed");
+    if(username.value.trim() == "") {
+        // alert("Blank username !");
+        username.style.border = "solid 3px red"
+        document.getElementById("lbluser").style.visibility="visible";
+        return false;
+    }
+    else if(password.value.trim() == "") {
+        alert("Blank Password !");
+        return false;
+    }
+    else if(password.value.trim().length<5) {
+        alert("Password is too short !");
         return false;
     }
     else {
-        true;
+        return true;
     }
 }
 
