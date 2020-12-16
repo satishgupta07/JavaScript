6bd7c1bc44cbfd410a5a18446cd0cf95d13a050d
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-16 11:23:18 +0530

    Taking Input from Radiobuttons
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,5 +1,17 @@
+//   DOM manipulation & getElementById method
+
 function fn1() {
-    var str = document.getElementById("text1").value;
-    alert("Value inside the text box is: "+str);
+    var rd1 = document.getElementById("rd1");
+    var rd2 = document.getElementById("rd2");
+    
+    if(rd1.checked == true) {
+        alert("The channel selected is : "+rd1.value);
+    }
+    else if(rd2.checked == true) {
+        alert("The channel selected is : "+rd2.value);
+    }
+    else {
+        alert("No channel selected");
+    }
 }
    
