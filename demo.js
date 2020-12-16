f7dcf71a76d6822133c880b5f304b685d1204fe0
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-16 12:02:45 +0530

    getElementByTagName
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,7 +1,15 @@
-//   DOM manipulation & getElementById method
+//   getElementsByTagName method
 
-function fn1() {
-    var select = document.getElementById("selectbox");
-    alert(select.options[select.selectedIndex].value)
+function changeStyling() {
+    var para = document.getElementsByTagName("p");
+    para[0].style.color = "blue";
+    para[1].style.color = "red";
+    para[2].style.fontWeight = "bold";
+    para[3].style.fontStyle = "italic";
+    para[4].style.color = "green";
+
+    for(var i=0; i<para.length; i++) {
+        para[i].style.fontSize = "25px";
+    }
 }
    
