c2d292eb00c0611731c8608fb2f88ceafc54e644
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-16 11:32:46 +0530

    Taking Input from SelectBox
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,17 +1,7 @@
 //   DOM manipulation & getElementById method
 
 function fn1() {
-    var rd1 = document.getElementById("rd1");
-    var rd2 = document.getElementById("rd2");
-    
-    if(rd1.checked == true) {
-        alert("The channel selected is : "+rd1.value);
-    }
-    else if(rd2.checked == true) {
-        alert("The channel selected is : "+rd2.value);
-    }
-    else {
-        alert("No channel selected");
-    }
+    var select = document.getElementById("selectbox");
+    alert(select.options[select.selectedIndex].value)
 }
    
