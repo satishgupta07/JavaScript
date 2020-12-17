d78391cf939dfd122b2b19c4fe1d5f1a1f1e51de
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-17 11:11:24 +0530

    Error-first callbacks
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- AsynchronousJavaScript/callbacks.js
+++ AsynchronousJavaScript/callbacks.js
@@ -1,33 +1,49 @@
 function download(url, downloaded) {
     console.log(`Downloading from ${url}`)
+    if(!url.startsWith('http')) {
+        return downloaded(new Error('URL must starts with http'))
+    }
     setTimeout(()=>{
         let savedFile = url.split('/').pop()
         console.log(`Downloaded and saved as ${savedFile}`)
-        downloaded(savedFile)
+        downloaded(null, savedFile)
     },2000)
 }
 
 function compress(file, format, compressed) {
     console.log(`Compressing ${file}`)
+    if(['zip', 'gzip', '7z'].indexOf(format) === -1) {
+        return compressed(new Error('We only support zip, gzip and 7z'))
+    }
     setTimeout(()=>{
         let archive = file.split('.')[0]+'.'+format
         console.log(`Compressed and saved as ${archive}`)
-        compressed(archive)
+        compressed(null, archive)
     },2000)
 }
 
 function upload(server, file, uploaded) {
     console.log(`Uploading ${file} to ${server}`)
+    if(!server.startsWith('ftp://')) {
+        return uploaded(new Error('We can only upload to FTP servers'))
+    }
     setTimeout(()=>{
         let remotePath = `${server}/${file}`
         console.log(`Uploaded to ${remotePath}`)
-        uploaded(remotePath)
+        uploaded(null, remotePath)
     },2000)
 }
 
-download("http://somesite.com/path/image.jpg", (file) => {
-    compress(file, 'zip', (archive) => {
-        upload('http://file.com', archive, () => {})
+download("http://somesite.com/path/image.jpg", (err, file) => {
+    if(err) throw err
+    compress(file, 'gzip', (err, archive) => {
+        if(err) {
+            console.warn(err)
+            archive = file
+        }
+        upload('ftp://file.com', archive, (err) => {
+            if(err) throw err
+        })
     })
 })
 
