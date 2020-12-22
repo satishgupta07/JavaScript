3af3748e305d4886fa760d80ab820f75edb82d72
Satish Kumar <satish.kumar06@nagarro.com>
2020-12-22 12:18:11 +0530

    Regular Expressions (Pattern Matching)
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- demo.js
+++ demo.js
@@ -1,26 +1,16 @@
-/* Form Validations in Javascript */
+//   Regular Expressions are used to perform powerful pattern-matching and 
+//   search-and-replace functions on text.
 
-function validate() {
-    var username = document.getElementById("uname");
-    var password = document.getElementById("pass");
-
-    if(username.value.trim() == "") {
-        // alert("Blank username !");
-        username.style.border = "solid 3px red"
-        document.getElementById("lbluser").style.visibility="visible";
-        return false;
-    }
-    else if(password.value.trim() == "") {
-        alert("Blank Password !");
-        return false;
-    }
-    else if(password.value.trim().length<5) {
-        alert("Password is too short !");
-        return false;
-    }
-    else {
-        return true;
-    }
-}
+  function validate() {
+      var username = document.getElementById("uname").value;
+      var regx = /E00/i;
+      if(regx.test(username)) {
+          alert("Valid Username");
+      }
+      else {
+          alert("Invalid Username");
+          document.getElementById('lbluser').style.visibility="visible";
+      }
+  }
 
    
